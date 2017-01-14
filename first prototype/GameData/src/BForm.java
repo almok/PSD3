2c48b6b7fcdb5a083fb68bdebcb4a9231cff9fb5
Ronaldas <2143806g@student.gla.ac.uk>
2017-01-14 20:42:35 +0000

    first prototype created. Form B created

---
+++ first prototype/GameData/src/BForm.java
@@ -0,0 +1,11 @@
+
+public class BForm {
+	
+	// fields
+	private int orderNo;
+	private String chassisType, productCode;
+	private float receiptTime, actDelTime, scheDelTime, scheLeadTime, delTime,
+		penalty, price, revenue;
+	
+	
+}
