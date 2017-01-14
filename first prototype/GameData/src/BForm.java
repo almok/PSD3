f1fcc7d8b7986f44c6423d90e89e0cc137d56eb2
Ronaldas <2143806g@student.gla.ac.uk>
2017-01-14 20:46:35 +0000

    Form B updated

--- first prototype/GameData/src/BForm.java
+++ first prototype/GameData/src/BForm.java
@@ -7,5 +7,29 @@ public class BForm {
 	private float receiptTime, actDelTime, scheDelTime, scheLeadTime, delTime,
 		penalty, price, revenue;
 	
+	// constructors
+	public BForm(int orderNo, String chassisType, String productCode, float price, float penalty, float receiptTime,
+			float actDelTime, float scheDelTime, float scheLeadTime){
+		this(orderNo, chassisType, productCode, price, penalty);
+		
+		this.receiptTime = receiptTime;
+		this.actDelTime = actDelTime;
+		this.scheDelTime = scheDelTime;
+		this.scheLeadTime = scheLeadTime;
+		delTime = scheDelTime - actDelTime;
+	}
+
+	public BForm(int orderNo, String chassisType, String productCode, float price, float penalty){
+		this(price, penalty);
+		
+		this.orderNo = orderNo;
+		this.chassisType = chassisType;
+		this.productCode = productCode;
+	}
 	
+	public BForm(float price, float penalty){
+		this.price = price;
+		this.penalty = penalty;
+		revenue = price - penalty;
+	}	
 }
