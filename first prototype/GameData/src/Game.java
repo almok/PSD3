fb9d743455b128005e919eee71c6dbd749a2192e
Ronaldas <2143806g@student.gla.ac.uk>
2017-01-15 20:20:50 +0000

    Game session file created

---
+++ first prototype/GameData/src/Game.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Game {
+	
+	// data structure to hold all B forms
+	ArrayList<BForm> bForms;
+	
+	
+	public Game(){
+		bForms = new ArrayList<BForm>();
+	}
+
+	public static void main(String[] args) {
+		
+		Game startNewGame = new Game();
+
+	}
+
+}
