06d1aa1dd74ff0ad3328b476bca82070e7d54a8a
Ronaldas <2143806g@student.gla.ac.uk>
2017-02-22 23:56:13 +0000

    kit price calculation added

--- forth prototype/src/OrderHistory.java
+++ forth prototype/src/OrderHistory.java
@@ -1,3 +1,4 @@
+import main.PSDSingleton;
 
 public class OrderHistory {
 
@@ -10,7 +11,7 @@ public class OrderHistory {
 		
 		orderNumber = order.getOrderNumber();
 		productCode = order.getProductCode();
-		kitPrice = calcKitPrice(order);
+		kitPrice = calcKitPrice();
 	}
 
 	// getter and setter
@@ -44,16 +45,55 @@ public class OrderHistory {
 	}
 	
 	// calculate kit price using product code
-	public double calcKitPrice(Order order){
+	public double calcKitPrice(){
+		String carName;
+		String chassis;
+		String prodLine;
+		char lights;
+		char gps;
 		
-		// todo
+		prodLine = Character.toString(productCode.charAt(0));
+		chassis = Character.toString(productCode.charAt(1));
+		chassis += Character.toString(productCode.charAt(2));
 		
-		return 3;
+		switch(prodLine){
+			case "F": case "f": carName = "Family "; break;
+			case "P": case "p": carName = "Pony "; break;
+			case "S": case "s": carName = "Sedan "; break;
+			case "A": case "a": carName = "Ayrton "; break;
+			case "C": case "c": carName = "Coast"; break;
+			case "T": case "t": carName = "Thunder"; break;	
+			default: return -1;
+		}
+		
+		if (prodLine == "C" || prodLine == "c" || prodLine == "T" || prodLine == "t"){
+			return Double.parseDouble(PSDSingleton.getInstance().getLightsPrice(carName));
+		}
+		else {
+			
+			lights = productCode.charAt(6);
+			gps = productCode.charAt(5);
+			
+			switch(chassis){
+				case "ST": case "st": case "St": case "sT": carName += "Standard"; break;
+				case "DE": case "de": case "Dr": case "dT": carName += "Dragstar"; break;
+				case "DR": case "dr": case "De": case "dE": carName += "Deluxe"; break;
+				default: return -1;
+			}
+		
+			if (lights == 'Y' && gps == '1'){
+				return Double.parseDouble(PSDSingleton.getInstance().getGPSYellowPrice(carName));
+			}
+			else if (lights == 'Y' && gps == '0'){
+				return Double.parseDouble(PSDSingleton.getInstance().getNoGPSYellowPrice(carName));
+			}
+			else if (lights == 'X' && gps == '1'){
+				return Double.parseDouble(PSDSingleton.getInstance().getGPSXenonPrice(carName));
+			}
+			else if (lights == 'X' && gps == '0'){
+				return Double.parseDouble(PSDSingleton.getInstance().getNoGPSXenonPrice(carName));
+			}
+		}
+		return -1;
 	}
-	
-	
-	
-	
-	
-	
 }
