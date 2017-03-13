35538bb97c87b6b922bd3b09e91256c40d8fd0fc
Ronaldas <2143806g@student.gla.ac.uk>
2017-03-13 19:56:35 +0000

    FormVcontroller test case created. Issue #39

---
+++ QPQ Game/src/tests/FormVTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import forms.FormVcontroller;
+
+public class FormVTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		FormVcontroller formV = new FormVcontroller();
+		
+	}
+
+	@Test
+	public void testDisplayTotal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveFields() {
+		fail("Not yet implemented");
+	}
+
+}
