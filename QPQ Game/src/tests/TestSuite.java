dc73d5f6187e012a5e068a15a9e00c06cd00a14d
Ronaldas <2143806g@student.gla.ac.uk>
2017-03-14 14:09:49 +0000

    Tests consolidated in a suite. Issue #39

---
+++ QPQ Game/src/tests/TestSuite.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AYNEmptest.class, FormOtest.class, OrderHistorytest.class })
+public class TestSuite {
+
+}
