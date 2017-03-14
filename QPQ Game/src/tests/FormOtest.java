3675672be1b39060179de10e7523e282a927196f
Ronaldas <2143806g@student.gla.ac.uk>
2017-03-14 11:36:37 +0000

    FormO tests updated. Issue #39

--- QPQ Game/src/tests/FormOtest.java
+++ QPQ Game/src/tests/FormOtest.java
@@ -1,24 +1,35 @@
 package tests;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import forms.FormOMenu;
-import main.PSDSingleton;
+import forms.Order;
 
 public class FormOtest {
 
+	private FormOMenu formO;
+	
 	@Before
-	public static void init() throws Exception {
-		PSDSingleton data = PSDSingleton.getInstance();
-		FormOMenu formO = new FormOMenu();
+	public void setUp(){
+		formO = new FormOMenu();
+		formO.updateOrderHistory(new Order(null, "FDR371Y", null, null, null, null));
+		formO.updateOrderHistory(new Order(null, "pst350y", null, null, null, null));
+		formO.updateOrderHistory(new Order(null, "aDe350X", null, null, null, null));
+		formO.updateOrderHistory(new Order(null, "ssT370Y", null, null, null, null));
+		formO.updateOrderHistory(new Order(null, "CsSB0y", null, null, null, null));
+		formO.updateOrderHistory(new Order(null, "dddddddddddddd", null, null, null, null));
+		formO.updateOrderHistory(new Order(null, "", null, null, null, null));
 	}
 
 	@Test
 	public void testCalcTotalKitPrice() {
-		fail("Not yet implemented");
+		
+		Double sum = 290.0 + 280.0 + 368.0 + 310.0 + 463.0;
+		assertEquals(sum, (Double) formO.calcTotalKitPrice());
+		
 	}
 
 }
