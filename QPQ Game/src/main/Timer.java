2361cd67640daa48a4aa7042f890d3f6c8ad4597
Tom Gough <2136487g@bo720-10-02u.dcs.gla.ac.uk>
2017-03-22 13:19:40 +0000

    Timer now counts up from 0 issue #61

--- QPQ Game/src/main/Timer.java
+++ QPQ Game/src/main/Timer.java
@@ -37,110 +37,16 @@ public class Timer extends Stage implements Initializable  {
 	private Label timeLabel;
 	
 	
-	
-	
+		
 	//@FXML static Button startTimer;
 		//private Label timeLabel = new Label();
 		private Timeline timeline;
-		private static final Integer STARTTIME = PSDSingleton.getInstance().getRoundTime();
+		private static final Integer STARTTIME = 0;
+		private static final Integer ENDTIME = PSDSingleton.getInstance().getRoundTime();
 		private Integer timeSeconds = 0;
-		private Integer timeMin = STARTTIME -1;
+		private Integer timeMin = STARTTIME + 1;
 		
-		 
-
-		
-
-		
-		
-		/*public Timer() throws IOException {
 			
-			
-			//Parent root = FXMLLoader.load(getClass().getResource("Timer.fxml"));
-			//.this.setTitle("Timer");
-			//Group root = new Group();
-			//Scene scene = new Scene(root, 800, 700);
-			
-			 timeLabel.setText(STARTTIME.toString() + ":00");
-		     timeLabel.setTextFill(Color.RED);
-		     timeLabel.setStyle("-fx-font-size: 4em;");
-			//Button resetTimer = new Button();
-			//resetTimer.setText("Reset");
-			resetTimer.setOnAction(new EventHandler<ActionEvent>(){
-				public void handle(ActionEvent event){
-					timeSeconds = 0;
-					timeMin = STARTTIME ;
-					timeLabel.setText(timeMin.toString()+ ":00");
-				}
-			});
-		    Button stopTimer = new Button();
-			stopTimer.setText("Stop");
-			stopTimer.setOnAction(new EventHandler<ActionEvent>(){
-				public void handle(ActionEvent event){
-					timeline.stop();
-				}
-			});
-			Button startTimer = new Button();
-			startTimer.setText("Start");
-			startTimer.setOnAction(new EventHandler<ActionEvent>(){
-				
-				
-				
-				public void handle(ActionEvent event) {
-					
-					
-					if (timeline != null){
-						System.out.println("timeline not null");
-						//timeline.stop();
-						
-					}else{
-					timeMin = STARTTIME;
-					timeLabel.setText(timeMin.toString() + ":00");
-					}
-					//update timer label
-					
-			        timeline = new Timeline();
-			        timeline.setCycleCount(Timeline.INDEFINITE);
-			        timeline.getKeyFrames().add(
-			        		new KeyFrame(Duration.seconds(1),
-			        				new EventHandler<ActionEvent>(){
-			        			//keyframe event handler
-			        			public void handle(ActionEvent event){
-			        				timeSeconds--;
-			        				System.out.println("seconds");
-			        				boolean isSecondsZero = timeSeconds <= 0;
-			        				
-			        				if(isSecondsZero){
-			        					timeMin--;
-			        					timeSeconds = 60;
-			        				}
-			        				//update timer label
-			        				timeLabel.setText(String.format("%d:%02d", timeMin, timeSeconds-1));
-			        				if (timeMin == 0 && timeSeconds <= 1){
-			        					timeline.stop();
-			        				}
-			        			}
-			        		}));
-					timeline.playFromStart();
-					System.out.println("pressed");
-				}
-			
-			});
-			/*VBox vb = new VBox(20);
-	        // center the components within VBox
-	        vb.setAlignment(Pos.CENTER);
-	        // Make it as wide as the application frame (scene)
-	        vb.setPrefWidth(scene.getWidth());
-	        // Move the VBox down a bit
-	        vb.setLayoutY(30);
-	        // Add the button and timerLabel to the VBox
-	        vb.getChildren().addAll(startTimer, timeLabel, stopTimer, resetTimer);
-	        // Add the VBox to the root component
-	        root.getChildren().add(vb);*/
-	        //scene.getStylesheets().add("forms/Styling.css");
-	        
-		//}
-		
-		
 		
 		public void display(Button button) throws IOException 
 		{
@@ -160,7 +66,7 @@ public class Timer extends Stage implements Initializable  {
 			
 			
 			
-			 timeLabel.setText(STARTTIME.toString() + ":00");
+			 timeLabel.setText("00:00");
 		     timeLabel.setTextFill(Color.RED);
 		     timeLabel.setStyle("-fx-font-size: 10em;");
 			//Button resetTimer = new Button();
@@ -169,7 +75,7 @@ public class Timer extends Stage implements Initializable  {
 				public void handle(ActionEvent event){
 					timeSeconds = 0;
 					timeMin = STARTTIME ;
-					timeLabel.setText(timeMin.toString()+ ":00");
+					timeLabel.setText("00:00");
 				}
 			});
 		    //Button stopTimer = new Button();
@@ -184,7 +90,7 @@ public class Timer extends Stage implements Initializable  {
 			startTimer.setOnAction(e -> {
 				
 				
-				System.out.println("START");
+				
 				//public void handle(ActionEvent event) {
 					
 					
@@ -194,7 +100,7 @@ public class Timer extends Stage implements Initializable  {
 						
 					}else{
 					timeMin = STARTTIME;
-					timeLabel.setText(timeMin.toString() + ":00");
+					timeLabel.setText("00:00");
 					}
 					//update timer label
 					
@@ -205,23 +111,23 @@ public class Timer extends Stage implements Initializable  {
 			        				new EventHandler<ActionEvent>(){
 			        			//keyframe event handler
 			        			public void handle(ActionEvent event){
-			        				timeSeconds--;
-			        				System.out.println("seconds");
-			        				boolean isSecondsZero = timeSeconds <= 0;
+			        				timeSeconds++;
 			        				
-			        				if(isSecondsZero){
-			        					timeMin--;
-			        					timeSeconds = 60;
+			        				boolean isSeconds60 = timeSeconds > 59;
+			        				
+			        				if(isSeconds60){
+			        					timeMin++;
+			        					timeSeconds = 0;
 			        				}
 			        				//update timer label
-			        				timeLabel.setText(String.format("%d:%02d", timeMin, timeSeconds-1));
-			        				if (timeMin == 0 && timeSeconds <= 1){
+			        				timeLabel.setText(String.format("%02d:%02d", timeMin, timeSeconds));
+			        				if (timeMin == ENDTIME){
 			        					timeline.stop();
 			        				}
 			        			}
 			        		}));
 					timeline.playFromStart();
-					System.out.println("pressed");
+					
 				//}
 			
 			});
